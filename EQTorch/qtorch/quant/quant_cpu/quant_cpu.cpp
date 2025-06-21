a53b3d572493e5b7747c92113878e43ef0580db4
ThoshithKautilya19 <143333995+ThoshithKautilya19@users.noreply.github.com>
2025-06-22 04:58:11 +0530

    Update quant_cpu.cpp

--- EQTorch/qtorch/quant/quant_cpu/quant_cpu.cpp
+++ EQTorch/qtorch/quant/quant_cpu/quant_cpu.cpp
@@ -835,6 +835,51 @@ Tensor configurable_table_quantize_rounding_hint(Tensor a, Tensor lookup_table, 
 }
 
 // Making thr fixed-posit , all definitions required and everything
+
+// #define FP16_LIMB_SIZE 16
+// #define FP16_TYPE uint16_t
+
+
+
+// #define SIGN_MASK 0x8000
+// #define FLOAT_SIGN_MASK 0x80000000
+// #define FLOAT_SIGN_RESET_MASK 0x7FFFFFFF
+// #define SECOND_BIT_MASK 0x4000
+// #define POSIT_INF 0x0000
+// #define POSIT_LIMB_ALL_BITS_SET 0xffff
+// #define SINGLE_PRECISION_BIAS 127
+// #define FLOAT_SIZE 32
+// #define FLOAT_EXPONENT_MASK 0x7f800000
+// #define FLOAT_FRACTION_MASK 0x007fffff
+// #define FLOAT_SIGN_SHIFT 31
+// #define FLOAT_EXPONENT_SHIFT 23
+// #define FLOAT_DENORMAL_EXPONENT -126
+// #define FLOAT_HIDDEN_BIT_SET_MASK 0x00800000
+// #define FLOAT_SIGN_PLUS_EXP_LENGTH_MINUS_ONE 8
+// #define TEMP_TYPE uint64_t
+// #define UNSIGNED_LONG_LONG_SIZE 64
+// #define EDP_ACC_SIZE 63
+// #define POSIT_EXP_SHIFT 41 //64-23
+// #define FLOAT_EXP_SIGN_SHIFT 30
+// #define FLOAT_INF 0x7F800000
+// #define FLOAT_SIGN_PLUS_EXP_LENGTH 9
+// #define POSIT_LENGTH_PLUS_ONE 17
+
+// #define GET_MAX(a, b)                                                          \
+//   ({                                                                           \
+//     __typeof__(a) _a = (a);                                                    \
+//     __typeof__(b) _b = (b);                                                    \
+//     _a > _b ? _a : _b;                                                         \
+//   })
+
+// #define _G_INFP 32768
+
+// union Bits {
+// 	float f;
+// 	int32_t si;
+// 	uint32_t ui;
+// };
+
 #define _FG_FPOSIT_SHIFT_AMOUNT   int32_fp_constants[0] //DONE
 #define _FG_MAXREALP             int32_fp_constants[1] //DONE
 #define _FG_MINREALP             int32_fp_constants[2] //DONE
